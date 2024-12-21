1c04535df6e4261f74dd81ed640c3377739fcedc
davy.dev <davy.andrade@pdcase.com.br>
2024-12-20 23:12:34 -0300

    Atualização da integração entre admin e catálogo

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const config = {
     catalogUrl: 'https://flow-principal.vercel.app',
-    adminUrl: 'https://flow-admin-one.vercel.app'
+    adminUrl: 'https://flow-admin-one.vercel.app',
+    apiUrl: 'https://flow-principal.vercel.app/api'
 };
 
-if (typeof module !== 'undefined' && module.exports) {
+if (typeof window !== 'undefined') {
+    window.config = config;
+} else if (typeof module !== 'undefined' && module.exports) {
     module.exports = config;
 }
